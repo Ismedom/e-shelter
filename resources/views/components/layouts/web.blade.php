b91146758263f4ce8d21214e9bf982da3f960352
nothing-full <mthimpse@gmail>
2025-04-21 00:11:10 +0700

    terms and privacy of service contributing

--- resources/views/components/layouts/web.blade.php
+++ resources/views/components/layouts/web.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.main>
     <x-common.nav-bar/>
-    
+    {{
+        $slot
+    }}
     
 </x-layouts.main>
---
+++ resources/views/website/term-condition/user.blade.php
@@ -0,0 +1,107 @@
+<x-layouts.web>
+    <section class ="p-5">
+        <h3 class="text-[rgb(162,170,173)] text-sm font-sans font-bold">Service User</h3>
+        <p class="text-[rgb(162,170,173)]">The service user to be aware as follow:</p>
+        <ol class="text-[rgb(162,170,173)] list-decimal list-inside italic">
+            <li>Account Registration</li>
+                <ol class="text-[rgb(162,170,173)] list-inside ml-6 space-y-1 mt-1">
+                    <li class="before:content-['1.1_'] before:mr-1">Requirements</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li> Provide accurate personal details (name, email, phone).</li>
+                            <li> Keep login credentials secure.</li>
+                        </ul>
+                    <li class="before:content-['1.2_'] before:mr-1">Prohibited</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>Fake accounts or impersonation.</li>
+                            <li>Sharing accounts with unauthorized users.</li>
+                        </ul>
+                </ol>
+            <li>Booking & Payments</li>
+                <ol class="text-[rgb(162,170,173)] list-inside ml-6 space-y-1 mt-1">
+                    <li class="before:content-['2.1_'] before:mr-1">Payment Terms:</li>
+                         <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>Prices include taxes/fees unless stated otherwise.</li>
+                            <li>Payment is processed immediately upon booking.</li>
+                            <li> Clear pricing (including taxes/fees)before selecting and paying.</li>
+                        </ul>
+                    <li class="before:content-['2.2_'] before:mr-1">Cancellation & Refunds</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>Follow the hotel’s cancellation policy (displayed before booking).</li>
+                            <li>Refunds processed within 7-14 business days.</li>
+                         </ul>
+                         <li class="before:content-['2.3_'] before:mr-1">No-Shows</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>Follow the hotel’s cancellation policy (displayed before booking).</li>
+                            <li>Late cancellations may incur charges (varies by hotel)</li>
+                         </ul>
+                </ol>
+            <li>User Responsibilities</li>
+                <ol class="text-[rgb(162,170,173)] list-inside ml-6 space-y-1 mt-1">
+                    <li class="before:content-['3.1_'] before:mr-1"> Respect Property Rules</li>
+                         <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li> Follow hotel policies (e.g., no smoking, noise restrictions).</li>
+                            <li> Report damages immediately.</li>
+                        </ul>
+                    <li class="before:content-['3.2_'] before:mr-1">Prohibited Actions</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>Illegal activities (drugs, theft, violence)..</li>
+                            <li>Unauthorized commercial use (e.g., parties without permission).</li>
+                         </ul>             
+                </ol>
+            <li>Platform Usage Rules</li>
+                <ol class="text-[rgb(162,170,173)] list-inside ml-6 space-y-1 mt-1">
+                    <li class="before:content-['4.1_'] before:mr-1"> Fair Use</li>
+                         <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li> Do not exploit bugs or manipulate prices.</li>
+                            <li> No fraudulent bookings or chargeback abuse.</li>
+                        </ul>
+                    <li class="before:content-['4.2_'] before:mr-1">Penalties</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>Account suspension for violations.</li>
+                            <li>Legal action for fraud or damages.</li>
+                         </ul>             
+                </ol>
+            <li> Liability & Disputes</li>
+                <ol class="text-[rgb(162,170,173)] list-inside ml-6 space-y-1 mt-1">
+                    <li class="before:content-['5.1_'] before:mr-1">Platform’s Role</li>
+                         <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li> We facilitate bookings but are not liable for Hotel service issues.</li>
+                            <li> We facilitate bookings but are not liable for Injuries, theft, or property damage.</li>
+                        </ul>
+                    <li class="before:content-['5.2_'] before:mr-1">User’s Responsibility</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>Resolve issues directly with the hotel first.</li>
+                            <li>Contact platform support for unresolved disputes.</li>
+                         </ul>             
+                </ol>
+            <li> Privacy & Data Use</li>
+                <ol class="text-[rgb(162,170,173)] list-inside ml-6 space-y-1 mt-1">
+                    <li class="before:content-['6.1_'] before:mr-1">Data Collection</li>
+                         <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li> We process data per our Privacy Policy.</li>
+                            <li> Payment details are securely encrypted.</li>
+                        </ul>
+                    <li class="before:content-['6.2_'] before:mr-1">Communications</li>
+                        <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li>You consent to booking confirmations and promotional emails.</li>
+                         </ul>             
+                </ol>
+             <li> Changes to Terms</li>
+                <!--<ol class="text-[rgb(162,170,173)] list-inside ml-6 space-y-1 mt-1">-->
+                    <!--<li class="before:content-['6.1_'] before:mr-1"></li>-->
+                         <ul class="list-disc list-inside ml-6 space-y-2">
+                            <li> We may update these terms; continued use means acceptance.</li>
+                            
+                        </ul>
+                
+        </ol>
+        <ul class="text-[rgb(162,170,173)] list-none list-inside italic"> 
+            <li class="font-bold"> Acceptance</li>
+            <li>By booking, you confirm:</li>
+            <li>You’ve read and agreed to these terms.</li>
+            <li>You are at least 18 years old.</li>
+            <li>You accept the hotel’s policies.</li>
+        </ul>
+    </section>
+
+</x-layouts.web>
